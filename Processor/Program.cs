9a7b4e1dc5fb45c748dbf11aa2cebe60f087f930
Robert Sundström <robert.sundstrom@outlook.com>
2022-04-10 14:40:59 +0200

    Tags

--- Processor/Program.cs
+++ Processor/Program.cs
@@ -2,13 +2,39 @@
 
 using ReadTimeEstimator.Implementations.Estimators;
 
-string dir = "../PersonalSite/wwwroot/";
+string wwwrootDir = "../PersonalSite/wwwroot/";
+string postsDir = $"{wwwrootDir}posts";
+string tagsDir = $"{wwwrootDir}tags";
 
 List<PostInfo> posts = new List<PostInfo>();
 
 var htmlEstimator = new HtmlEstimator();
 
-foreach(var file in Directory.GetFiles($"{dir}posts", "*.md"))
+try 
+{
+    Directory.CreateDirectory(postsDir);
+}
+catch(Exception) {}
+
+try 
+{
+    Directory.CreateDirectory(tagsDir);
+}
+catch(Exception) {}
+
+foreach (var file in Directory.GetFiles(postsDir, "*.json"))
+{
+    File.Delete(file);
+}
+
+foreach (var file in Directory.GetFiles(tagsDir, "*.json"))
+{
+    File.Delete(file);
+}
+
+Dictionary<string, List<PostInfo>> tags = new();
+
+foreach (var file in Directory.GetFiles(postsDir, "*.md"))
 {
     Console.WriteLine(file);
 
@@ -17,7 +43,7 @@ foreach(var file in Directory.GetFiles($"{dir}posts", "*.md"))
 
     var frontMatter = MarkdownExtensions.GetFrontMatter<BlogFrontMatter>(text);
 
-    if(frontMatter is null)
+    if (frontMatter is null)
         continue;
 
     var content = MarkdownExtensions
@@ -28,7 +54,23 @@ foreach(var file in Directory.GetFiles($"{dir}posts", "*.md"))
     var estimatedTime = TimeSpan.FromMinutes(
         htmlEstimator.ReadTimeInMinutes(content));
 
-    posts.Add(new PostInfo(frontMatter.Title, frontMatter.Subtitle, fileName, frontMatter.Author, frontMatter.Image, frontMatter.Published, estimatedTime, excerpt, frontMatter.Tags));
+    var post = new PostInfo(frontMatter.Title, frontMatter.Subtitle, fileName, frontMatter.Author, frontMatter.Image, frontMatter.Published, estimatedTime, excerpt, frontMatter.Tags);
+
+    posts.Add(post);
+
+    if (frontMatter.Tags is not null)
+    {
+        foreach (var tag in frontMatter.Tags)
+        {
+            if (!tags.TryGetValue(tag, out var tagPosts))
+            {
+                tagPosts = new List<PostInfo>();
+                tags[tag] = tagPosts;
+            }
+
+            tagPosts.Add(post);
+        }
+    }
 }
 
 var chunks = posts.OrderByDescending(x => x.Published).Chunk(5);
@@ -36,15 +78,39 @@ var chunks = posts.OrderByDescending(x => x.Published).Chunk(5);
 int i = 1;
 var count = chunks.Count();
 
-foreach(var chunk in chunks) 
+foreach (var chunk in chunks)
 {
-    var postsJson = JsonSerializer.Serialize(new IndexPage(chunk, i, count));
+    var postsJson = JsonSerializer.Serialize(new IndexPage(null, chunk, i, count));
 
-    await File.WriteAllTextAsync($"{dir}posts/_index-{i}.json", postsJson);
+    await File.WriteAllTextAsync($"{postsDir}/_index-{i}.json", postsJson);
 
     i++;
 }
 
-record IndexPage(IEnumerable<PostInfo> Posts, int Page, int TotalPages);
+foreach (var tag in tags)
+{
+    var chunks2 = tag.Value.OrderByDescending(x => x.Published).Chunk(5);
+
+    i = 1;
+    count = chunks2.Count();
+
+    foreach (var chunk in chunks2)
+    {
+        var postsJson = JsonSerializer.Serialize(new IndexPage(tag.Key, chunk, i, count));
+
+        await File.WriteAllTextAsync($"{tagsDir}/{tag.Key}-{i}.json", postsJson);
+
+        i++;
+    }
+}
+
+foreach(var tag in tags) 
+{
+    var postsJson = JsonSerializer.Serialize(tags.ToDictionary(x => x.Key, x => x.Value.Count()));
+
+    await File.WriteAllTextAsync($"{tagsDir}/index.json", postsJson);
+}
+
+record IndexPage(string? Name, IEnumerable<PostInfo> Posts, int Page, int TotalPages);
 
 record PostInfo(string Title, string? Subtitle, string Slug, string Author, string? Image, DateTime Published, TimeSpan? EstimatedReadTime, string Excerpt, string[] Tags);

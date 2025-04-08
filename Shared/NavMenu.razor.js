1af1a339d76a50bb75f7a4631210c2797c196abe
marinasundstrom <marna.li@outlook.com>
2025-04-08 18:54:48 +0000

    Deploying to gh-pages from @ marinasundstrom/marinas-blog@e2339e3633a2afd8cb819f10836eeb2fd4a718f9 🚀

--- Shared/NavMenu.razor.js
+++ Shared/NavMenu.razor.js
@@ -1,4 +1,4 @@
-export function init() {        
+export function init() {
     const offCanvasCollapse = document.querySelector('.offcanvas-collapse');
 
     // Toggle off canvas when clicking the menu toggle
@@ -9,8 +9,7 @@ export function init() {
     // Toggle offcanvas off when clicking links
     document.querySelectorAll('a.nav-link')
         .forEach(linkElement => {
-            if(linkElement.classList.contains("dropdown-toggle"))
-            {
+            if (linkElement.classList.contains("dropdown-toggle")) {
                 // Toggle offcanvas when clicking the items, 
                 // but ignore clicking the dropdown itself
 
@@ -24,7 +23,7 @@ export function init() {
 
             linkElement.addEventListener('click', () => {
                 offCanvasCollapse.classList.toggle('open');
-            }) 
+            })
         });
 
     const dropdown = new bootstrap.Dropdown(document.querySelector(".theme-selector .dropdown-toggle"));
@@ -37,4 +36,28 @@ export function init() {
 
 export function initCM() {
     initColorModes();
+}
+
+export function scrollableNavbar() {
+    const navbar = document.querySelector(".navbar");
+    const navbarCollapse = document.querySelector(".navbar-collapse");
+
+    const updateNavbarBackground = () => {
+        if (window.scrollY > 1) {
+            navbar.classList.add("shadow-sm");
+            navbar.classList.add("bg-brand");
+            navbarCollapse.classList.add("bg-brand");
+            navbarCollapse.classList.remove("navbar-transparent");
+        } else {
+            navbar.classList.remove("shadow-sm");
+            navbar.classList.remove("bg-brand");
+            navbarCollapse.classList.remove("bg-brand");
+            navbarCollapse.classList.add("navbar-transparent");
+        }
+    };
+
+    window.addEventListener("scroll", updateNavbarBackground);
+    window.addEventListener("DOMContentLoaded", updateNavbarBackground);
+
+    updateNavbarBackground();
 }

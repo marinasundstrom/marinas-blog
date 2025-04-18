41e9f7baa9c29b1e6cb7e9d253922eaaa8d5b4d3
Marina Sundström <marna.li@outlook.com>
2025-04-18 14:09:23 +0200

    Fix TOC prerendering

--- PersonalSite/Shared/TableOfContents.razor.cs
+++ PersonalSite/Shared/TableOfContents.razor.cs
@@ -1,6 +1,7 @@
 using System.Runtime.InteropServices.JavaScript;
 
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.JSInterop;
 
 namespace PersonalSite.Shared;
@@ -11,8 +12,13 @@ public partial class TableOfContents
 
     [Inject] public IJSRuntime JS { get; set; }
 
+    [Inject] IWebAssemblyHostEnvironment HostEnv { get; set; }
+
     protected override async Task OnInitializedAsync()
     {
+        if (HostEnv.Environment == "Prerendering")
+            return;
+
         module = await JS.InvokeAsync<IJSObjectReference>("import",
             "../Shared/TableOfContents.razor.js");
 
